8043c68f4cc29709f28992902d4e709eecc0149e
Kai-Cheng Mei <kennymei47@gmail.com>
2024-11-10 20:48:58 -0800

    update1

--- main.cpp
+++ main.cpp
@@ -98,35 +98,38 @@ int main() {
 
     // Start a time-based simulation for city growth and changes
         // Do 25 time period(25 years)
-            // Go through each city in the map
-                // For each city, apply changes based on the event:
-                    // little change:
-                    // random add people or delete (if radom % 100 : <60 +1 people >60 -1 people)
-                    // random add facility or delete (if radom % 100 : <40 +1 facility >40 -1 facility)
-                    // random add animal or delete (if radom % 100 : <50 +1 animal >50 -1 animal)
-                    
-                    // Call function simulate(what is in function)
-                        // If "Economic Boom" (20%)
-                            // Add new resident to the resident list
-                            // Add new facilities to the facility list
-                            // Reduce animals in the animal list
-
-                        // If "Economic Depression" (10%)
-                            // Remove residents from the resident list
-                            // Remove  facilities from the facility list
-                            // Add animals to the animal list
-
-                        // If "Government Build Factory" (15%)
-                            // Remove residents from the resident list
-                            // Remove facilities in the facility list
-                            // Reduce animals in the animal list
-
-                    // Print the update ove the time,  "Added {number} residents to {city}" or "Closed {facility} in {city}"
-
+        for (int i = 1, i < 25, i++) {
+            cout << "------Year: " << year << endl;
+            simulate(cities);
+            display;
+            
         // Every 1 year, print the new information of three city
 
     // End of main function
 }
 
 // void simulate(const map<string, array<list<string>, 3>>& cities) 
-// 
+    // Go through each city in the map
+            // For each city, apply changes based on the event:
+                // little change:
+                // random add people or delete (if radom % 100 : <60 +1 people >60 -1 people)
+                // random add facility or delete (if radom % 100 : <40 +1 facility >40 -1 facility)
+                // random add animal or delete (if radom % 100 : <50 +1 animal >50 -1 animal)
+                
+                // Call function simulate(what is in function)
+                    // If "Economic Boom" (20%)
+                        // Add new resident to the resident list
+                        // Add new facilities to the facility list
+                        // Reduce animals in the animal list
+
+                    // If "Economic Depression" (10%)
+                        // Remove residents from the resident list
+                        // Remove  facilities from the facility list
+                        // Add animals to the animal list
+
+                    // If "Government Build Factory" (15%)
+                        // Remove residents from the resident list
+                        // Remove facilities in the facility list
+                        // Reduce animals in the animal list
+
+                // Print the update ove the time,  "Added {number} residents to {city}" or "Closed {facility} in {city}"
